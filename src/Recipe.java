89be34a772c82eebc623b7a28d334e8599bd90d2
alexg60 <alexg6@umbc.edu>
2024-12-08 21:26:11 -0500

    Update Recipe.java

--- src/Recipe.java
+++ src/Recipe.java
@@ -5,24 +5,41 @@ public class Recipe {
     private String recipeInstructions;
     private List<String> ingredients;
 
+    /**
+     *
+     * @param name
+     * @param ingredient
+     * @param instructions
+     */
     public Recipe(String name, List<String> ingredient, String instructions){
         this.recipeName = name;
         this.ingredients = ingredient;
         this.recipeInstructions = instructions;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getRecipeName(){
         return recipeName;
     }
 
+    /**
+     *
+     * @return
+     */
     public List<String> getIngredients(){
         return ingredients;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getRecipeInstructions(){
         return recipeInstructions;
     }
 
 }
-
 
